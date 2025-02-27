333aed29c3498079437b372c7150bcfe21fb269c
Melike Seda Ketenci <115991225+MSedaK@users.noreply.github.com>
2025-02-27 18:49:21 +0300

    Main game fix cool down - +

--- Assets/Scripts/PortalSpawner.cs
+++ Assets/Scripts/PortalSpawner.cs
@@ -91,7 +91,7 @@ public class PortalSpawner : MonoBehaviour
 
     float GetEnemySpeedForWave(int waveNumber)
     {
-        return 5f + (waveNumber - 1) * 2f;
+        return 7f + (waveNumber - 1) * 2f;
     }
 
     EnemyWaveData GetWaveData(int wave)
