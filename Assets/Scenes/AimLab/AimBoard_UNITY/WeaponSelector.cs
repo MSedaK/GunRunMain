dc03f921412218aa34a98f165e615a51ec9cb35a
Melike Seda Ketenci <115991225+MSedaK@users.noreply.github.com>
2025-03-10 18:43:23 +0300

    AimLab
    Aimlab silah seçim UI güncellendi, yeni silah eklendi ve tutorial info yazısı daktilodan yazılır gibi yenilendi

--- Assets/Scenes/AimLab/AimBoard_UNITY/WeaponSelector.cs
+++ Assets/Scenes/AimLab/AimBoard_UNITY/WeaponSelector.cs
@@ -2,38 +2,56 @@ using UnityEngine;
 
 public class WeaponSelector : MonoBehaviour
 {
-    public GameObject Canik;
-    public GameObject Silencer;
-    public GameObject Shotgun;
+    public GameObject GlockR;
+    public GameObject GlockL;
+    public GameObject Pyt;
+    public GameObject Shotty;
+    public GameObject Minimach;
 
     private GameObject activeWeapon;
 
     void Start()
     {
-        activeWeapon = Canik;
-        Canik.SetActive(true);
-        Silencer.SetActive(false);
-        Shotgun.SetActive(false);
+        activeWeapon = GlockR;
+        GlockR.SetActive(true);
+        GlockL.SetActive(true);
+        Pyt.SetActive(false);
+        Shotty.SetActive(false);
+        Minimach.SetActive(false);
     }
 
     public void SelectWeapon(string weaponName)
     {
         if (activeWeapon != null)
         {
-            activeWeapon.SetActive(false);
+            if (activeWeapon == GlockR || activeWeapon == GlockL)
+            {
+                GlockR.SetActive(false);
+                GlockL.SetActive(false);
+            }
+            else
+            {
+                activeWeapon.SetActive(false);
+            }
         }
 
-        if (weaponName.Contains("Canik"))
+        if (weaponName.Contains("Glock"))
         {
-            activeWeapon = Canik;
+            activeWeapon = GlockR;  
+            GlockR.SetActive(true);
+            GlockL.SetActive(true);
         }
-        else if (weaponName.Contains("Silencer"))
+        else if (weaponName.Contains("Pyt"))
         {
-            activeWeapon = Silencer;
+            activeWeapon = Pyt;
         }
-        else if (weaponName.Contains("Shotgun"))
+        else if (weaponName.Contains("Shotty"))
         {
-            activeWeapon = Shotgun;
+            activeWeapon = Shotty;
+        }
+        else if (weaponName.Contains("Minimach"))
+        {
+            activeWeapon = Minimach;
         }
 
         activeWeapon.SetActive(true);
